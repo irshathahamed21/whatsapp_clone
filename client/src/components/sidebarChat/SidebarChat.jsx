f52480ac96bfc0e0c2990d80de01d5dca0885456
irshathahamed21 <21irshathkhan@gmail.com>
2022-05-05 23:46:35 +0530

    sidebarChat component added

--- client/src/components/sidebarChat/SidebarChat.jsx
+++ client/src/components/sidebarChat/SidebarChat.jsx
@@ -3,19 +3,29 @@ import React , {useEffect,useState}from 'react'
 import "./sidebarchat.css"
 import {Avatar} from "@mui/material"
 
-function SidebarChat() {
+function SidebarChat({addNewChat}) {
     const[seed, setSeed] = useState()
 
     useEffect(() => {
         setSeed(Math.floor(Math.random()*5000))
     },[])
 
-  return (
-      <div className="sidebarchat">
+  return !addNewChat ? (
+      <div className="sidebarChat">
           <Avatar 
           src = {`https://avatars.dicebear.com/api/human/${seed}.svg`}
           />
+          <div className="sidebarChat__info">
+              <h1>heloo</h1>
+          </div>
       </div>
+  ) : (
+    <div className="sidebarChat">
+
+    <h1>add new chat</h1>
+    
+    </div>
+
   )
 }
 
