b0a9ff7c5f2e2752ea0846aef830267ee7be414d
irshathahamed21 <21irshathkhan@gmail.com>
2022-05-05 23:43:17 +0530

    chat component folder added

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,8 +1,83 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {Avatar,  IconButton} from "@mui/material"
+import {MoreVert, SearchOutlined , AttachFile, InsertEmoticon} from "@mui/icons-material";
+import "./chat.css"
+
 
 function Chat() {
+
+  const[seed, setSeed] = useState()
+
+  useEffect(() => {
+      setSeed(Math.floor(Math.random()*5000))
+  },[])
+
+
   return (
-    <div>Chat</div>
+    <div className="chat">
+
+      <div className="chat__header">
+      <Avatar  src = {`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+
+
+      <div className="chat__headerInfo">
+        <h1>React developers</h1>
+        <p>Last updated at {new Date().toString().slice(0,25)}</p>
+      </div>
+
+      <div className="chat__headerRight">
+                <IconButton>
+                    <SearchOutlined/>
+                </IconButton>
+                <IconButton>
+                    <AttachFile/>
+                </IconButton>
+                <IconButton>
+                    <MoreVert/>
+                </IconButton>
+      </div>
+
+      </div>
+
+      <div className="chat__body">
+       <p className="chat__message">
+        
+         <span className="chat__name">
+          Irshath ahamed
+         </span>
+         Hello from irshath
+         <span className="chat__timeStamp">
+           {new Date().toString().slice(0,25)}
+
+         </span>
+
+       </p>
+       <p className="chat__message ">
+        
+        <span className="chat__name chat__receiver">
+         Irshath ahamed
+        </span>
+        Hello from irshath
+        <span className="chat__timeStamp">
+          {new Date().toString().slice(0,25)}
+
+        </span>
+
+      </p>
+
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticon/>
+        <form>
+        <input type="text" placeholder = "start a new message" />
+        <button>send message</button>
+        </form>
+       
+
+      </div>
+
+      
+    </div>
   )
 }
 
